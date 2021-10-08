41e01922cc7d8dbc7f45a9a71d9961bb3ecc11d3
Daniel Stang <dstang14@gmail.com>
2021-10-08 19:01:19 +0000

    Add basic unit tests.

---
+++ unittests.py
@@ -0,0 +1,17 @@
+import unittest
+import bookclub
+
+class TestBookclub(unittest.TestCase):
+  def test_status_str(self):
+    # Check that correct numbers output string.
+    self.assertEqual(type(bookclub.status_str('-1')), str)
+    self.assertEqual(type(bookclub.status_str('0')), str)
+    self.assertEqual(type(bookclub.status_str('25')), str)
+    self.assertEqual(type(bookclub.status_str('50')), str)
+    self.assertEqual(type(bookclub.status_str('75')), str)
+    self.assertEqual(type(bookclub.status_str('100')), str)
+    self.assertEqual(bookclub.status_str('99'), None)
+    self.assertEqual(bookclub.status_str('28'), None)
+
+if __name__ == '__main__':
+    unittest.main()
