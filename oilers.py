8e124de27a4fa4e6d6336e27a66774d71c672c15
wustangdan <3793229-wustangdan@users.noreply.replit.com>
2023-05-04 01:10:53 +0000

    Oilers playoff updates

--- oilers.py
+++ oilers.py
@@ -44,35 +44,47 @@ class OilersTracker:
         return row.split(',')[4:8]
 
     def get_game_id_and_title(self, api):
+        in_playoffs = True
+        game_found = False
         if 'previousGameSchedule' in api['teams'][0]:
             info = api['teams'][0]['previousGameSchedule']['dates'][0][
                 'games'][0]
+            game_found = True
         if 'nextGameSchedule' in api['teams'][0]:
             info = api['teams'][0]['nextGameSchedule']['dates'][0]['games'][0]
+            game_found = True
+        if game_found == False:
+            return '', ''
         away = info['teams']['away']
         home = info['teams']['home']
         # Away team name and record.
         title = away['team']['name'] + ' '
         if 'previousGameSchedule' in api['teams'][0]:
-            # Avoid spoilers by hiding record of old game.
-            title += '|| '
+            # Record of old game.
+            title += ' '
         title += '(' + str(away['leagueRecord']['wins']) + '-'
         title += str(away['leagueRecord']['losses']) + '-'
-        title += str(away['leagueRecord']
-                     ['ot']) + ') '  # Doesn't appear during playoffs.
+        if not in_playoffs:
+            title += str(away['leagueRecord']
+                         ['ot']) + ') '  # Doesn't appear during playoffs.
+        else: 
+            title += ') '
         if 'previousGameSchedule' in api['teams'][0]:
-            title += ' ||'
+            title += ' '
         title += 'at '
         # Home team name and record.
         title += home['team']['name'] + ' '
         if 'previousGameSchedule' in api['teams'][0]:
-            title += '|| '
+            title += ' '
         title += '(' + str(home['leagueRecord']['wins']) + '-'
         title += str(home['leagueRecord']['losses']) + '-'
-        title += str(home['leagueRecord']
-                     ['ot']) + ')'  # Doesn't appear during playoffs.
+        if not in_playoffs:
+            title += str(home['leagueRecord']
+                         ['ot']) + ')'  # Doesn't appear during playoffs.
+        else:
+            title += ')'
         if 'previousGameSchedule' in api['teams'][0]:
-            title += ' ||'
+            title += ' '
         title += ' - ' + info['status']['abstractGameState']
         return str(info['gamePk']), title
 
@@ -102,28 +114,32 @@ class OilersTracker:
         return projected_points, make_playoffs, make_final, win_cup, win_cup_rank
 
     def print_next_game_time(self):
+        in_playoffs = True
         oilers_next = self.api_team_next()
         oilers_next = oilers_next['teams'][0]
-        oilers_next = oilers_next['nextGameSchedule']['dates'][0]['games'][0]
-        next_game_str = oilers_next['teams']['away']['team']['name']
-        next_game_str += ' at '
-        next_game_str += oilers_next['teams']['home']['team']['name']
-        next_game_str += ', '
-        date = datetime.strptime(oilers_next['gameDate'], '%Y-%m-%dT%H:%M:%SZ')
-        date -= timedelta(hours=7)
-        next_game_str += date.__str__()[:-3] + ' MT'
+        next_game_str = ''
+        if "nextGameSchedule" in oilers_next:
+            oilers_next = oilers_next['nextGameSchedule']['dates'][0]['games'][0]
+            next_game_str += oilers_next['teams']['away']['team']['name']
+            next_game_str += ' at '
+            next_game_str += oilers_next['teams']['home']['team']['name']
+            next_game_str += ', '
+            date = datetime.strptime(oilers_next['gameDate'], '%Y-%m-%dT%H:%M:%SZ')
+            date -= timedelta(hours=6)
+            next_game_str += date.__str__()[:-3] + ' MT'
         # Regular Season
-        projected_points, make_playoffs, make_final, win_cup, win_cup_rank = self.stats_projection(
-        )
-        next_game_str += '\n`\nEDM Stats Projection (fivethirtyeight.com)'
-        next_game_str += '\nProj. Points / Make Playoffs: ' + projected_points + ' / ' + make_playoffs
-        next_game_str += '\nWin Cup Rank / Win Cup %:     ' + win_cup_rank + ' / ' + win_cup + '`'
-
+        if not in_playoffs:
+            projected_points, make_playoffs, make_final, win_cup, win_cup_rank = self.stats_projection(
+            )
+            next_game_str += '\n`\nEDM Stats Projection (fivethirtyeight.com)'
+            next_game_str += '\nProj. Points / Make Playoffs: ' + projected_points + ' / ' + make_playoffs
+            next_game_str += '\nWin Cup Rank / Win Cup %:     ' + win_cup_rank + ' / ' + win_cup + '`'
         # Playoffs
-        #round1, round2, round3, cup = self.playoffs_money_puck()
-        #next_game_str += '\n`\nEDM Odds Projection (moneypuck.com)'
-        #next_game_str += f'\nWin Round 1: {float(round1)*100:.1f}% | Win Round 2: {float(round2)*100:.1f}%'
-        #next_game_str += f'\nWin Conference: {float(round3)*100:.1f}% | Win Cup: {float(cup)*100:.1f}%`'
+        else:
+            round1, round2, round3, cup = self.playoffs_money_puck()
+            next_game_str += '\n`\nEDM Odds Projection (moneypuck.com)'
+            next_game_str += f'\nWin Round 1: {float(round1)*100:.1f}% | Win Round 2: {float(round2)*100:.1f}%'
+            next_game_str += f'\nWin Conference: {float(round3)*100:.1f}% | Win Cup: {float(cup)*100:.1f}%`'
         return next_game_str
 
     def print_game_info(self, game_id, title):
@@ -133,8 +149,7 @@ class OilersTracker:
         # Needed since I potentially use recap in description.
         description = ''
         if len(game['media']['epg'][3]['items']) >= 1:
-            description = '||' + game['media']['epg'][3]['items'][0][
-                'title'] + '||'
+            description = '' + game['media']['epg'][3]['items'][0]['title'] + ''
         embed = discord.Embed(title=title,
                               description=description,
                               color=discord.Colour.orange())
@@ -147,7 +162,7 @@ class OilersTracker:
         for highlight_index in sorted(highlight_order):
             highlight = game['highlights']['gameCenter']['items'][
                 highlight_index[1]]
-            highlight_name = '||' + highlight['blurb'].split(': ')[-1] + '||'
+            highlight_name = '' + highlight['blurb'].split(': ')[-1] + ''
             embed.add_field(name=highlight_name,
                             value="[Link](" +
                             highlight['playbacks'][3]['url'] + ")",
@@ -163,12 +178,14 @@ class OilersTracker:
                 game['media']['epg'][2]['items'][0]['playbacks'][3]['url'] +
                 ')',
                 inline=False)
+            # playoffs not supplied
             embed.add_field(name='Next Game',
                             value=self.print_next_game_time())
 
             return embed, True
 
     async def write_discord_msg(self, game_id, game_embed, finished, channel):
+        in_playoffs = True
         if game_embed == None:
             # Nothing to output.
             return
@@ -183,19 +200,23 @@ class OilersTracker:
             # Message exists, edit existing message.
             msg = await channel.fetch_message(self.replit_db[game_id])
             await msg.edit(embed=game_embed)
-            if finished is True:
+            if finished is True and in_playoffs is False:
                 self.replit_db[game_id] = 'finished'
 
     async def run(self, channel):
         # TODO Add proper error handling if no nextgameschedule.
         # Get prev game ID and attempt to output.
         prev_game_id, title = self.get_game_id_and_title(self.api_team_prev())
+        if title == '':
+            return
         prev_embed, prev_finished = self.print_game_info(prev_game_id, title)
         await self.write_discord_msg(prev_game_id, prev_embed, prev_finished,
                                      channel)
 
         # Get next game ID and attemp to output.
         next_game_id, title = self.get_game_id_and_title(self.api_team_next())
+        if title == '':
+            return
         next_embed, next_finished = self.print_game_info(next_game_id, title)
         await self.write_discord_msg(next_game_id, next_embed, next_finished,
                                      channel)
