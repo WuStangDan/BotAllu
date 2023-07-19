567e92690f19c2b0442f9ed83e66f4d35b89eab0
WuStangDan <dstang14@gmail.com>
2023-07-19 06:54:38 +0000

    GPT formatting files.

--- bookclub.py
+++ bookclub.py
@@ -4,40 +4,52 @@ from tabulate import tabulate
 
 def parse_message(msg):
     """
-    parse_message determines which internal function to pass the bookclub message too.
+    Determines which internal function to pass the book club message to.
 
-    return: False indicates failure and to keep message and send warning. String indicates sucess and leaderboard should be updated.
+    Args:
+        msg (str): The book club message.
+
+    Returns:
+        bool or str: False indicates failure and to keep the message and send a warning. A string indicates success and the leaderboard should be updated.
     """
-    if msg[0:4] == "add ":
+    if msg.startswith("add "):
         return add(msg[4:])
-    elif msg[0:6] == "[add] ":
+    elif msg.startswith("[add] "):
         return add(msg[6:])
-    elif msg[0:7] == "update ":
+    elif msg.startswith("update "):
         return update(msg[7:])
-    elif msg[0:7] == "remove ":
+    elif msg.startswith("remove "):
         return remove(msg[7:])
-    elif msg[0:9] == "[remove] ":
+    elif msg.startswith("[remove] "):
         return remove(msg[9:])
-
     return False
 
 
 def status_str(num):
+    """
+    Converts a status number to a corresponding status string.
+
+    Args:
+        num (int or str): The status number.
+
+    Returns:
+        str: The corresponding status string.
+    """
     # Filter out square brackets.
-    if type(num) == str:
+    if isinstance(num, str):
         num = num.split("[")[-1]
         num = num.split("]")[0]
 
     num = int(num)
     if num == -1:
         return ":x:"
-    if num >= 0 and num < 25:
+    if 0 <= num < 25:
         return ":clock12:"
-    if num >= 25 and num < 50:
+    if 25 <= num < 50:
         return ":clock3:"
-    if num >= 50 and num < 75:
+    if 50 <= num < 75:
         return ":clock6:"
-    if num >= 75 and num < 100:
+    if 75 <= num < 100:
         return ":clock9:"
     if num == 100:
         return ":white_check_mark:"
@@ -45,17 +57,20 @@ def status_str(num):
 
 
 def progress():
+    """
+    Generates the progress string representing the leaderboard.
+
+    Returns:
+        str: The progress string.
+    """
     progress_str = "`" + db["bookclub"]["game_name"] + "\n"
     # Add underline.
-    for i in range(len(progress_str)):
-        progress_str += "_"
-    progress_str += "`\n"
+    progress_str += "_" * len(progress_str) + "`\n"
 
-    d = db["bookclub"]["players"]
-    for player in d:
-        progress_str += status_str(d[player]) + " "
-        progress_str += "`" + player
-        progress_str += "`\n"
+    players = db["bookclub"]["players"]
+    for player in players:
+        progress_str += status_str(players[player]) + " "
+        progress_str += "`" + player + "`\n"
 
     progress_str += "\n` * !bookclub newgame GAMENAME"
     progress_str += "\n * !bookclub add PLAYER"
@@ -66,7 +81,13 @@ def progress():
 
 def add(player):
     """
-    add Places a discord user into the board.
+    Places a Discord user into the leaderboard.
+
+    Args:
+        player (str): The name of the player.
+
+    Returns:
+        str: The updated progress string representing the leaderboard.
     """
     db["bookclub"]["players"][player] = -1
     return progress()
@@ -74,16 +95,32 @@ def add(player):
 
 def remove(player):
     """
-    add Removes a discord user from the board.
+    Removes a Discord user from the leaderboard.
+
+    Args:
+        player (str): The name of the player.
+
+    Returns:
+        bool or str: False if the player is not found, otherwise the updated progress string representing the leaderboard.
     """
-    if player in db["bookclub"]["players"]:
-        del db["bookclub"]["players"][player]
+    players = db["bookclub"]["players"]
+    if player in players:
+        del players[player]
         return progress()
     else:
         return False
 
 
 def update(player_status):
+    """
+    Updates the status of a player in the leaderboard.
+
+    Args:
+        player_status (str): The player name and status in the format 'PLAYER#STATUS'.
+
+    Returns:
+        bool or str: False if the input format is invalid or the status is unknown, otherwise the updated progress string representing the leaderboard.
+    """
     player_status = player_status.split("#")
 
     # Verify input contains player name and status.
@@ -92,8 +129,8 @@ def update(player_status):
     player = player_status[0]
     status = player_status[1]
 
-    # Check that status is sensical.
-    if status_str(status) == None:
+    # Check that status is sensible.
+    if status_str(status) is None:
         return False
     db["bookclub"]["players"][player] = status
     return progress()
